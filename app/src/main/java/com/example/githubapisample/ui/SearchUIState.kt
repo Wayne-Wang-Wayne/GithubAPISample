b92090389ba504d88478eae5fadbbf844b1ce7c7
Wayne <w702897028970289@gmail.com>
2024-03-19 17:30:37 +0800

    調整SearchUIState

--- app/src/main/java/com/example/githubapisample/ui/SearchUIState.kt
+++ app/src/main/java/com/example/githubapisample/ui/SearchUIState.kt
@@ -3,9 +3,14 @@ package com.example.githubapisample.ui
 import com.example.githubapisample.data.remotedata.RepoData
 
 data class SearchUIState(
-    val isLoading: Boolean = true,
-    val isError: Boolean = false,
+    val stateType: StateType = StateType.LOADING,
     val errorMessage: String = "",
     val searchQuery: String = "",
     val repositories: List<RepoData> = emptyList()
 )
+
+enum class StateType {
+    LOADING,
+    ERROR,
+    SUCCESS
+}
